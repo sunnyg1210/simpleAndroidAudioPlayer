c546640eebf958c4f229ac121671951e0b09ac25
Resheph Gohil <comp_sunny68@hotmail.com>
2017-02-09 15:33:30 +0000

    Initial Commit

---
+++ app/src/main/java/com/svsdomaincontrol/simpleaudioplayer/MainActivity.java
@@ -0,0 +1,107 @@
+package com.svsdomaincontrol.simpleaudioplayer;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button play;
+    Button stop;
+    Button pause;
+    MediaPlayer mp;
+    SeekBar seekbar;
+    Handler handler;
+    Runnable runnable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = (Button)findViewById(R.id.play);
+        pause = (Button)findViewById(R.id.pause);
+        stop = (Button)findViewById(R.id.stop);
+        seekbar = (SeekBar)findViewById(R.id.pos);
+        handler = new Handler();
+
+        mp = MediaPlayer.create(this, R.raw.song);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+                playCycle();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp.isPlaying()) {
+                    mp.pause();
+                    mp.seekTo(0);
+                    seekbar.setProgress(0);
+                    handler.removeCallbacks(runnable);
+                }
+            }
+        });
+    }
+
+    public void play() {
+        mp.start();
+
+        seekbar.setMax(mp.getDuration());
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mp.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void playCycle() {
+        seekbar.setProgress(mp.getCurrentPosition());
+
+        if (mp.isPlaying()) {
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    playCycle();
+                }
+            };
+            handler.postDelayed(runnable, 1000);
+        }
+    }
+
+}
